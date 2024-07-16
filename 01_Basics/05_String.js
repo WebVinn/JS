f77163a85450c6db676db936080ed82350a2425f
Vineet Suryawanshi <vineetsuryawanshi07@gmail.com>
2024-07-16 08:12:23 +0000

    Discuss on String

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,22 @@
+let num1 = "VIneet";
+let num2 = 27;
+console.log(`My name is ${num1} and my age is ${num2}`);
+
+
+const newNum = new String('Vineet');
+console.log(newNum);
+console.log(newNum[0]);
+console.log(newNum.length);
+console.log(newNum.toUpperCase());
+console.log(newNum.charAt(3));
+console.log(newNum.indexOf('t'));
+console.log(newNum.substring(0, 4));
+console.log(newNum.replace('eet' , 'nie'));
+console.log(newNum.includes('Vineet'));
+console.log(newNum.includes('Vineetttt'));
+console.log(newNum.split('ee'));
+
+
+
+const newNumb = new String('    Vineet    ')
+console.log(newNumb.trim());
