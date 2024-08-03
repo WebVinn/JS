21ac8094a70a0ed4b9c86991c34afa0177fc7471
Vineet Suryawanshi <vineetsuryawanshi07@gmail.com>
2024-08-03 19:36:52 +0000

    Discuss on Arrow Functions

--- 03_Basics/03_Arrow.js
+++ 03_Basics/03_Arrow.js
@@ -7,4 +7,38 @@ const user = {
     }
 } 
 user.welcomeMessage()
+user.username = "Vinnie"
+user.welcomeMessage()
+
+
 console.log(this);
+
+
+const one = function () {
+    user : "Vineet"
+    console.log(this.user);
+    
+}
+one();
+
+
+const two = () => {
+    user : "Vineet"
+    console.log(this.user);
+    
+}
+two();
+
+
+// const twoNum = (num1,num2) => {
+//     return num1+num2;
+// }
+
+// const twoNum = (num1,num2) => num1+num2;
+
+// const twoNum = (num1,num2) => (num1+num2);
+
+const twoNum = (num1,num2) => ({user:"Vineet"})
+
+
+console.log(twoNum(2,3));
